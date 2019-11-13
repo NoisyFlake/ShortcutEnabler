443632a6aa943506c9621b5ebd579d8b6b8acfe3
ryannair05 <ryannair05@gmail.com>
2019-11-12 19:48:02 -0500

    Support for iOS 13

--- Tweak.h
+++ Tweak.h
@@ -1,3 +1,5 @@
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+
 @interface UIView (SpringBoardAdditions)
 - (void)sb_removeAllSubviews;
 @end
@@ -5,4 +7,8 @@
 @interface SBDashBoardQuickActionsView : UIView
 - (void)_layoutQuickActionButtons;
 - (void)handleButtonPress:(id)arg1 ;
+@end
+
+@interface CSQuickActionsView : UIView
+- (void)_layoutQuickActionButtons;
 @end
